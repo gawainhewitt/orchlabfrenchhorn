3628d82bd1f1d4640623fb7b602f159dcff5ef28
gawainhewitt <gawain@gawainhewitt.co.uk>
2021-01-14 11:31:27 +0000

    Menu change and build details
    Added a default to the drop down menu.
    
    Changed the git details in the build files

--- sketch - basic touch commands.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('p').addEventListener('touchstart', f);
-document.querySelector('p').addEventListener('touchend', f);
-document.querySelector('p').addEventListener('touchmove', f);
-document.querySelector('p').addEventListener('touchcancel', f);
-
-function f(ev){
-    ev.preventDefault();
-    console.log( ev.touches, ev.type );
-}
